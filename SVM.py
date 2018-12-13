e0e3ab1e36c735547ca7383c42356dfd45b66b90
joruizrodriguez <32525136+joruizrodriguez@users.noreply.github.com>
2018-12-12 21:28:24 -0500

    Update SVM.py

--- SVM.py
+++ SVM.py
@@ -2,22 +2,25 @@ import pandas as pd
 from sklearn.model_selection import train_test_split  
 from sklearn.svm import SVC  
 
+
 #Import dataset
 dataset = pd.read_csv('reviews.csv')
 
-#Split the data (D,classes)
+#Separate the data (Data,classes)
 Data = dataset.drop('class', axis=1)  
 classes = dataset['class']
 
 #Split the data and do resampling
-Data_train, Data_test, classes_train, classes_test = train_test_split(Data,classes, test_size = 0.10)  
+Data_train, Data_test, classes_train, classes_test = train_test_split(Data,classes, test_size = 0.20)  
 
-#Apply SVC classifier using a linear model
+#Apply SVC classifier using a linear kernel
 classifier = SVC(kernel='linear')  
-#Fit the data using the train data
+
+#Fit the data using train data
 classifier.fit(Data_train, classes_train)
+
 #Predict class labels of test 
-classes_pred = classifier.predict(Data_test)  
+classes_pred = classifier.predict(Data_test) 
 
 
 classes_pred = classes_pred.tolist()
@@ -30,7 +33,7 @@ for i in range(0, classes_size):
     if(classes_pred[i] == classes_test[i]):
         count += 1
 
-accuracy = float(count)/classes_size
+precision = float(count)/classes_size
 
 print "Accuracy: %s" % precision
-print classes_test
+print classes_pred
